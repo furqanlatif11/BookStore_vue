d6c3863a772a952bbf131f8d74ee9b2595af89aa
furqanlatif11 <furqanlatif615@gmail.com>
2023-09-27 04:05:19 -0700

    Latest Updates

--- src/store/index.js
+++ src/store/index.js
@@ -1,26 +1,4 @@
 // Utilities
-// import { createPinia } from 'pinia'
-// src/store/index.js
+import { createPinia } from 'pinia'
 
-import Vue from "vue";
-import Vuex from "vuex";
-import { createStore } from 'vuex';
-
-Vue.use(Vuex);
-
-export default new Vuex.Store({
-  state: {
-    // Define your application state variables here
-  },
-  mutations: {
-    // Define functions to modify the state here
-  },
-  actions: {
-    // Define actions here to commit mutations
-  },
-  getters: {
-    // Define getters to retrieve state variables
-  },
-});
-
-
+export default createPinia()
